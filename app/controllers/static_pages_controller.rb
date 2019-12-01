295288a71563f02cfef0350cb699f9ae25c202a6
willi <williboelke@outlook.com>
2019-12-01 16:54:36 +0100

    Rails conforme links in header und footer / static_pages controller mit actions about, contact und help hinzugefügt

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+  def contact
+  end
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+
+  #root
   root 'startpages#index'
-  get 'startpages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #startpage
+  get "startpage" => "startpages#index", as: "startpage"
+
+  #static_pages
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/help'
+
+  get "about" => "static_pages#about", as: "about"
+  get "contact" => "static_pages#contact", as: "contact"
+  get "help" => "static_pages#help", as: "help"
 end
