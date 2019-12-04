9ade69cfdb3cd023d662b6ec2a31df3e8cb0ac8a
willi <williboelke@outlook.com>
2019-12-04 23:56:21 +0100

    added Bullets in config/enviornments/development.rb ,/ added SimpleCov require in test_helper

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+  # watch your queries while you develop your application and notify you
+  # when you should add eager loading (N+1 queries),
+  # when you use eager loading that is not necessary - and
+  # when you should use counter caching
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.alert = false
+    Bullet.add_footer = false
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
+require 'simplecov'
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
