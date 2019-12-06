6e1d9b7a6407cf93a37bba4a8745863fdb882f6d
willi <williboelke@outlook.com>
2019-12-06 16:20:19 +0100

    Header und Footer implementiert / static_pages controller mit actions help, contact und about implementiert / code style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
+  get 'static_pages/help'
+  get 'static_pages/contact'
+  #root
+  root 'shops#index'
+
+  #shop
+  get "shop" => "shops#index", as: "shop"
+
+  #static_pages
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/help'
+
+  get "about" => "static_pages#about", as: "about"
+  get "contact" => "static_pages#contact", as: "contact"
+  get "help" => "static_pages#help", as: "help"
 end
