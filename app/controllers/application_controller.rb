7856202f25eb305c484e499ae840a3b04bd21d16
Willi Boelke <williboelke@outlook.com>
2020-01-16 21:50:25 +0100

    implemented language switch / routing and language files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
 
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
+
+
+  def default_url_options(options = {})
+    {locale: I18n.locale}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,21 @@
 Rails.application.routes.draw do
-
-
   root 'products#index'
 
-  devise_for :users
-  #root
-  root 'shops#index'
+  scope":locale" do
+
+    resources :products
+
+    #shop
+    get "shop" => "products#index", as: "shop"
+    get "sort" => "products#sort", as: "sort"
+    get "filter" => "products#filter", as: "filter"
+    get "search" => 'products#search', :as => "search"
+
+    #staticpages
+    get "about" => "static_pages#about", as: "about"
+    get "contact" => "static_pages#contact", as: "contact"
+    get "help" => "static_pages#help", as: "help"
+  end
 
 
-  resources :products
-
-  #shop
-  get "shop" => "products#index", as: "shop"
-
-  get "about" => "static_pages#about", as: "about"
-
-  get "sort" => "products#sort", as: "sort"
-
-  get "filter" => "products#filter", as: "filter"
-
-  get "search" => 'products#search', :as => "search"
-
-
-  #staticpages
-  get "contact" => "static_pages#contact", as: "contact"
-
-  get "help" => "static_pages#help", as: "help"
 end
