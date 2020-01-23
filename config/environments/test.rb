f4246cd124fcb9d2a58e2a9808ccdab0ac0b1c86
Willi Boelke <williboelke@outlook.com>
2020-01-23 21:40:03 +0100

    Added name and Surname to the user model

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   }
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
@@ -46,17 +46,19 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host =>'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "test.yourhost.com" }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 '587',
-    domain:               ENV['MAIL_HOST'],
-    user_name:            ENV['SENDMAIL_USERNAME'],
-    password:             ENV['SENDMAIL_PASSWORD'],
-    authentication:       'login',
-    enable_starttls_auto: true
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['SENDMAIL_USERNAME'],
+    password: ENV["SENDMAIL_PASSWORD"]
   }
 
 end
