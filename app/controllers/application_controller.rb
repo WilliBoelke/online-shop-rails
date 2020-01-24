01b5ff6527349c037244cda56cd363c9028e652a
Willi Boelke <williboelke@outlook.com>
2020-01-24 15:30:52 +0100

    Added user name and surname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
+# frozen_string_literal: true
 class ApplicationController < ActionController::Base
-  before_action :configure_devise_params, if: :devise_controller?
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_devise_params
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :surname, :email, :password, :password_confirmation)
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :surname, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:edit) do |user_params|
+      user_params.permit(:name, :surname, :email, :password, :password_confirmation, :current_password)
     end
   end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,13 @@ Rails.application.routes.draw do
 
   #shop
   get "shop" => "products#index", as: "shop"
-
-  get "about" => "static_pages#about", as: "about"
-
   get "sort" => "products#sort", as: "sort"
-
   get "filter" => "products#filter", as: "filter"
-
   get "search" => 'products#search', :as => "search"
 
 
   #staticpages
   get "contact" => "static_pages#contact", as: "contact"
-
+  get "about" => "static_pages#about", as: "about"
   get "help" => "static_pages#help", as: "help"
 end
