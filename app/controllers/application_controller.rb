91c37ab195839b33adee39b504169f2bc8216a32
Willi Boelke <williboelke@outlook.com>
2020-01-24 15:41:30 +0100

    merge fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,9 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-=======
-  before_action :configure_devise_params, if: :devise_controller?
-
-  def configure_devise_params
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :surname, :email, :password, :password_confirmation)
-    end
-  end
-
->>>>>>> f4246cd124fcb9d2a58e2a9808ccdab0ac0b1c86
 
   protected
 
@@ -26,4 +15,5 @@ class ApplicationController < ActionController::Base
       user_params.permit(:name, :surname, :email, :password, :password_confirmation, :current_password)
     end
   end
+
 end
