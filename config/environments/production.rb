b1c72eb8e81462858fe872f4d28a3466085a27f5
Willi Boelke <williboelke@outlook.com>
2020-02-11 15:26:38 +0100

    Heroku / sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,16 +111,17 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   #
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => ENV['HOST_URL'] }
-  config.action_mailer.smtp_settings = {
+  host = ENV['HOST_URL']
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
-    port: '25',
-    domain: 'heroku.com',
+    port: '587',
+    authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    authentication:       'plain',
+    domain: 'heroku.com',
     enable_starttls_auto: true
   }
 
