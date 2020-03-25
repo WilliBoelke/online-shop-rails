c963f3e6d9817a9c5f2d5553acd000ab48e8328c
Willi Boelke <williboelke@outlook.com>
2020-03-25 16:13:04 +0100

    removed email Regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   rolify
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # VALID_PASSWORD_REGEX = /^(?=.*\d)(?=.*([a-z]|[A-Z]))([\x20-\x7E]){8,40}$/
 
   before_save { email.downcase! }
   after_create :assign_default_role
   validates :name, presence: true, length: { minimum: 3, maximum: 20 }
   validates :surname, presence: true, length: { minimum: 3, maximum: 20 }
   validates :email, presence: true, length: { maximum: 255 }
-  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :email, uniqueness: true
   validates :password, presence: true, length: { minimum: 8, maximum: 40 }
 
 
